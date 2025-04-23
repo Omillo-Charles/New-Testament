9f7b19dc6aea45802bb4f4edcc3837f9152f2bc0
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-04-23 16:51:40 +0300

    The sign up form under the reports section

--- new-testament/src/Contents/Clergy.jsx
+++ new-testament/src/Contents/Clergy.jsx
@@ -4,6 +4,7 @@ import Meetings from '../assets/Meetings.jpeg'
 import Reports from '../assets/Reports.jpeg'
 import './Clergy.css'
 import Ministries from '../Ministries';
+import Reportss from '../TheChurchBranch/Reports';
 
 function Clergy() {
     const [activeContent, setActiveContent] = useState('');
@@ -15,7 +16,7 @@ function Clergy() {
         case 'Meetings':
           return <h1>Pastoral Meetings.</h1>;
         case 'Reports':
-          return <h1>Reports Section.</h1>
+          return <Reportss />
         default:
           return null;
       }
