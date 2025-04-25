28801ba6972df6752a1cd64d4b55bfcae3137b29
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-04-25 20:03:49 +0300

    Favicon

--- new-testament/src/Components/Navbar.jsx
+++ new-testament/src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
-import Favicon from '../assets/favicon.png'
+import Favicon from '../assets/Favicon.png'
 
 const Navbar = () => {
   return (
