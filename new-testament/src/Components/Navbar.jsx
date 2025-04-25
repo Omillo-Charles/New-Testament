293b91fede221c962e940c939bdfc012eac1d244
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-04-25 19:55:53 +0300

    Navbar favicon

--- new-testament/src/Components/Navbar.jsx
+++ new-testament/src/Components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
+import Favicon from '../assets/favicon.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="heading">
         <h2 className='PC'>NEW-TESTAMENT</h2>
-        <h2 className="mobile">NT.</h2>
+        <h2 className="mobile">
+          <img src={Favicon} alt="" />
+        </h2>
       </div>
 
       <div className="navLinks">
