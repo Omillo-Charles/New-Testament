b41d5f0bdd5f7b8f42ad90e7d621a814546ea559
Maruti Rodney <rodneymaruti@gmail.com>
2025-11-20 17:49:12 +0300

    remove social auth option

--- app/register/page.js
+++ app/register/page.js
@@ -290,18 +290,7 @@ export default function Register() {
             </div>
           </form>
 
-          <div className="mt-6">
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-300" />
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white text-gray-500">Or register with</span>
-              </div>
-            </div>
 
-
-          </div>
         </div>
 
         <div className="mt-8 text-center">
